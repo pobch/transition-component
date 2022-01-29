9d7b299efe05eb93f78cc3539aac499cee8df734
Pob <590650@gmail.com>
2022-01-30 00:29:02 +0700

    Implement <Transition/> and getDerivedStateFromProps

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import { useState } from 'react'
+import { Transition } from './Transition'
+import './App.css'
+
 export function App() {
+  const [activeScreen, setActiveScreen] = useState('0')
+  function nextScreen() {
+    // There are 3 screens
+    const newActiveScreen = String((Number(activeScreen) + 1) % 3)
+    setActiveScreen(newActiveScreen)
+  }
+
   return (
     <>
       <h1>Howdy</h1>
+      <div>
+        <button onClick={nextScreen}>Next</button>
+      </div>
+      <Transition className="transition" activeScreen={activeScreen}>
+        <div key="0" className="screen0">
+          Screen 0
+        </div>
+        <div key="1" className="screen1">
+          Screen 1
+        </div>
+        <div key="2" className="screen2">
+          Screen 2
+        </div>
+      </Transition>
     </>
   )
 }
---
+++ src/Transition.tsx
@@ -0,0 +1,65 @@
+import * as React from 'react'
+import { useState } from 'react'
+
+export function Transition({
+  children,
+  className,
+  activeScreen,
+}: {
+  children: React.ReactNode
+  className: string
+  activeScreen: string
+}) {
+  /**
+   * * Implement `getDerivedStateFromProps`. It consists of...
+   * * 1. "prop" which is a source of a derived state, i.e. `activeScreen`
+   * * 2. "state" to track previous value of "prop in 1.", i.e. `prevActiveScreen`
+   * * 3. "derived state" that will be calculated from "prop in 1." and "state in 2.", i.e. `exitingScreen`
+   */
+  const [prevActiveScreen, setPrevActiveScreen] = useState('')
+  const [exitingScreen, setExitingScreen] = useState('')
+  if (prevActiveScreen !== activeScreen) {
+    setExitingScreen(prevActiveScreen)
+    setPrevActiveScreen(activeScreen)
+  }
+  // * End of `getDerivedStateFromProps` implementation
+
+  function resetExitingScreen() {
+    setExitingScreen('')
+  }
+
+  return (
+    <div className={className} onTransitionEnd={resetExitingScreen}>
+      {React.Children.map(children, (child) => {
+        // Type check
+        if (React.isValidElement(child)) {
+          // Find the state of each specific screen
+          let screenState: 'active' | 'exiting' | 'inactive' = 'inactive'
+          if (child.key === activeScreen) {
+            screenState = 'active'
+          } else if (child.key === exitingScreen) {
+            screenState = 'exiting'
+          } else {
+            screenState = 'inactive'
+          }
+
+          // Display logic
+          switch (screenState) {
+            case 'active':
+              return <div key={child.key}>{child}</div>
+            case 'exiting':
+              return (
+                <div key={child.key} className="exit">
+                  {child}
+                </div>
+              )
+            case 'inactive':
+              return null
+          }
+        }
+        // Wrong type, not a ReactElement
+        return null
+      })}
+    </div>
+  )
+}
